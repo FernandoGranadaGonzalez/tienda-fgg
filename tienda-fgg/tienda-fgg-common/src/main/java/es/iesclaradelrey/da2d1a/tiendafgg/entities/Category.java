3713d968902f62ec9f57626f15a9a7f1f249f2e0
Fernando Granada González <97793959+FernandoGranadaGonzalez@users.noreply.github.com>
2026-04-17 10:21:29 +0200

    Archivo Category
    Category.java creado

---
+++ tienda-fgg/tienda-fgg-common/src/main/java/es/iesclaradelrey/da2d1a/tiendafgg/entities/Category.java
@@ -0,0 +1,17 @@
+package es.iesclaradelrey.da2d1a.tiendafgg.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String nombreImagen;
+}
