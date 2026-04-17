269573fee110f3bf1077d49e493ccf793a851342
Fernando Granada González <97793959+FernandoGranadaGonzalez@users.noreply.github.com>
2026-04-17 09:35:02 +0200

    Commit inicial
    Primer commit con la estructura creada (parent-common-web)

---
+++ tienda-fgg/tienda-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendafgg/tiendafgg/web/WebApp.java
@@ -0,0 +1,11 @@
+package es.iesclaradelrey.da2d1a.tiendafgg.tiendafgg.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "es.iesclaradelrey.da2d1a.tiendafgg")
+public class WebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
---
+++ tienda-fgg/tienda-fgg-web/src/test/java/es/iesclaradelrey/da2d1a/tiendafgg/tiendafgg/web/TiendaFggWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendafgg.tiendafgg.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFggWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
