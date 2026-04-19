89b658e060a2e98b24387107383e2bf8dcbbb75a
Fernando Granada González <97793959+FernandoGranadaGonzalez@users.noreply.github.com>
2026-04-19 22:44:15 +0200

    WebConfig creado

---
+++ tienda-fgg/tienda-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendafgg/tiendafgg/web/config/WebConfig.java
@@ -0,0 +1,14 @@
+package es.iesclaradelrey.da2d1a.tiendafgg.tiendafgg.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro) {
+        registro.addViewController("/condiciones").setViewName("legal/terms");
+    }
+}
