12b66d90cddf6011b9e85b6f0dc5685858fa1831
Fernando Granada González <97793959+FernandoGranadaGonzalez@users.noreply.github.com>
2026-04-29 20:51:41 +0200

    Listado de videojuegos y pathvariable creados

---
+++ tienda-fgg/tienda-fgg-web/src/main/java/es/iesclaradelrey/da2d1a/tiendafgg/web/controllers/VideojuegoController.java
@@ -0,0 +1,42 @@
+package es.iesclaradelrey.da2d1a.tiendafgg.web.controllers;
+
+import es.iesclaradelrey.da2d1a.tiendafgg.common.entities.Videojuego;
+import es.iesclaradelrey.da2d1a.tiendafgg.common.services.VideojuegoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/productos")
+public class VideojuegoController {
+
+    private final VideojuegoService videojuegoService;
+
+    public VideojuegoController(VideojuegoService videojuegoService) {
+        this.videojuegoService = videojuegoService;
+    }
+
+    @GetMapping
+    public String listarProductos(Model model) {
+        List<Videojuego> videojuegos = videojuegoService.obtenerTodos();
+        model.addAttribute("videojuegos", videojuegos);
+        return "productos/listado";
+    }
+
+    @GetMapping("/{id}/{nombreEscapado}")
+    public String verDetalle(@PathVariable("id") Long id, Model model) {
+        Optional<Videojuego> videojuegoOptional = videojuegoService.buscarPorId(id);
+
+        if (videojuegoOptional.isPresent()) {
+            model.addAttribute("juego", videojuegoOptional.get());
+            return "productos/detalle";
+        } else {
+            return "redirect:/productos";
+        }
+    }
+}
